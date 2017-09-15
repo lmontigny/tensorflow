b19f1a6b6030d5eaed452523c50da111a751d0bd
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-09-15 12:26:41 +0200

    Update addition.py

--- tutorials/addition.py
+++ tutorials/addition.py
@@ -1,5 +1,13 @@
+#Basic way
 import tensorflow as tf
 a = tf.add(3, 5)
 sess = tf.Session()
 print sess.run(a)
 sess.close()
+
+#Better way
+import tensorflow as tf
+a = tf.add(3, 5)
+sess = tf.Session()
+with tf.Session() as sess:
+print sess.run(a)
